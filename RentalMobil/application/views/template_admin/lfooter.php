7512b2eb90bfc3fa4902e22faac7b76c722349a8
DivanLaviantopamungkas <91887969+DivanLaviantopamungkas@users.noreply.github.com>
2024-06-25 17:11:52 +0700

    Delete RentalMobil directory

--- RentalMobil/application/views/template_admin/lfooter.php
+++
@@ -1,6 +0,0 @@
- <!-- JS -->
- <script src="<?php echo base_url('assets/assets_customer') ?>/vendor/jquery/jquery.min.js"></script>
- <script src="<?php echo base_url('assets/assets_customer') ?>/js/main.js"></script>
- </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
-
- </html>
